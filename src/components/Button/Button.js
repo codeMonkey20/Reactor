9be44dcd97376227c9f32bcff59f7061e2d5199c
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-06-23 12:53:11 +0800

    new button types added
    added types:
    1. raised
    2. stroked
    3. flat
    
    deleted types:
    1. icon

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -10,6 +10,8 @@ export default function Button({
   uppercase,
   hoverColor,
   disabled,
+  loadingAnim,
+  props,
 }) {
   const [isMouseHeld, setIsMouseHeld] = useState(false);
 
@@ -20,25 +22,30 @@ export default function Button({
           className={`
             rounded-md
             ${
-              disabled ? 
-                `
+              disabled || loadingAnim
+                ? `
                   bg-gray-200 text-gray-400
                   ${className ? className : `py-2 px-5`}
                   ${bold ? `font-bold` : ``}
                   ${uppercase ? `uppercase` : ``}
                 `
-                : 
-                `
+                : `
                   ${isMouseHeld ? `opacity-60` : ``}
                   ${className ? className : `py-2 px-5`}
                   ${bold ? `font-bold` : ``}
                   ${uppercase ? `uppercase` : ``}
-                  ${color ? `text-white bg-[${color}]` : "text-black"}
-                  ${hoverColor ? `hover:bg-[${hoverColor}]` : ``}
+                  ${color ? `text-white bg-[${color}]` : `text-black bg-white`}
+                  ${hoverColor ? `hover:text-white hover:bg-[${hoverColor}]` : ``}
                 `
             }
           `}
-          onClick={onClick}
+          onClick={
+            onClick && !loadingAnim
+              ? onClick
+              : () => {
+                  return;
+                }
+          }
           onMouseDown={() => {
             setIsMouseHeld(true);
           }}
@@ -46,35 +53,140 @@ export default function Button({
             setIsMouseHeld(false);
           }}
           disabled={disabled === true}
+          {...props}
         >
+          {loadingAnim ? <i className="mr-2 fa fa-circle-notch fa-spin" /> : ""}
           {children}
         </button>
       );
 
     case "raised":
       return (
-        <button className={`p-2`} onClick={onClick}>
+        <button
+          className={`
+            rounded-md drop-shadow-md
+            ${
+              disabled || loadingAnim
+                ? `
+                  bg-gray-200 text-gray-400
+                  ${className ? className : `py-2 px-5`}
+                  ${bold ? `font-bold` : ``}
+                  ${uppercase ? `uppercase` : ``}
+                `
+                : `
+                  ${isMouseHeld ? `opacity-60` : ``}
+                  ${className ? className : `py-2 px-5`}
+                  ${bold ? `font-bold` : ``}
+                  ${uppercase ? `uppercase` : ``}
+                  ${color ? `text-white bg-[${color}]` : `text-black bg-white`}
+                  ${hoverColor ? `hover:text-white hover:bg-[${hoverColor}]` : ``}
+                `
+            }
+          `}
+          onClick={
+            onClick && !loadingAnim
+              ? onClick
+              : () => {
+                  return;
+                }
+          }
+          onMouseDown={() => {
+            setIsMouseHeld(true);
+          }}
+          onMouseUp={() => {
+            setIsMouseHeld(false);
+          }}
+          disabled={disabled === true}
+          {...props}
+        >
+          {loadingAnim ? <i className="mr-2 fa fa-circle-notch fa-spin" /> : ""}
           {children}
         </button>
       );
 
     case "stroked":
       return (
-        <button className={`p-2`} onClick={onClick}>
+        <button
+          className={`
+            rounded-md border border-gray-300
+            ${
+              disabled || loadingAnim
+                ? `
+                  bg-gray-200 text-gray-400
+                  ${className ? className : `py-2 px-5`}
+                  ${bold ? `font-bold` : ``}
+                  ${uppercase ? `uppercase` : ``}
+                `
+                : `
+                  ${isMouseHeld ? `opacity-60` : ``}
+                  ${className ? className : `py-2 px-5`}
+                  ${bold ? `font-bold` : ``}
+                  ${uppercase ? `uppercase` : ``}
+                  ${hoverColor ? `hover:text-white hover:bg-[${hoverColor}]` : ``}
+                `
+            }
+          `}
+          onClick={
+            onClick && !loadingAnim
+              ? onClick
+              : () => {
+                  return;
+                }
+          }
+          onMouseDown={() => {
+            setIsMouseHeld(true);
+          }}
+          onMouseUp={() => {
+            setIsMouseHeld(false);
+          }}
+          disabled={disabled === true}
+          {...props}
+        >
+          {loadingAnim ? <i className="mr-2 fa fa-circle-notch fa-spin" /> : ""}
           {children}
         </button>
       );
 
     case "flat":
       return (
-        <button className={`p-2`} onClick={onClick}>
-          {children}
-        </button>
-      );
-
-    case "icon":
-      return (
-        <button className={`p-2`} onClick={onClick}>
+        <button
+          className={`
+            rounded-md
+            ${
+              disabled || loadingAnim
+                ? `
+                  text-gray-400
+                  ${className ? className : `py-2 px-5`}
+                  ${bold ? `font-bold` : ``}
+                  ${uppercase ? `uppercase` : ``}
+                `
+                : `
+                  ${isMouseHeld ? `opacity-60` : ``}
+                  ${className ? className : `py-2 px-5`}
+                  ${bold ? `font-bold` : ``}
+                  ${uppercase ? `uppercase` : ``}
+                  ${color ? `text-[${color}]` : `text-black`}
+                  ${hoverColor ? `hover:text-white hover:bg-[${hoverColor}]` : ``}
+                `
+            }
+          `}
+          onClick={
+            onClick && !loadingAnim
+              ? onClick
+              : () => {
+                  return;
+                }
+          }
+          onMouseDown={() => {
+            setIsMouseHeld(true);
+          }}
+          onMouseUp={() => {
+            setIsMouseHeld(false);
+          }}
+          disabled={disabled === true}
+          {...props}
+        >
+          {loadingAnim ? <i className="mr-2 fa fa-circle-notch fa-spin" /> : ""}
           {children}
         </button>
       );
